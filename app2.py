80085119522354db3e8c713e1d8dd6bdead97f62
Mmora-07 <mmora@unal.edu.co>
2025-07-13 23:43:36 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1sPGP69uedNGpZRm_md2wsUdPXh6QxMKv
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
